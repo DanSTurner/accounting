e6bec473703ca4b6a7befa136586e96544e281f0
Jonan Scheffler <jonanscheffler@gmail.com>
2013-11-19 17:35:06 -0800

    Add test runner, gemfile, gitignore and cleanup indentation.

--- test/expense_test.rb
+++ test/expense_test.rb
@@ -2,14 +2,15 @@ require 'minitest/autorun'
 require './lib/expense.rb'
 
 class ExpenseTest < MiniTest::Unit::TestCase
-def test_has_amount
-  expense = Expense.new({:amount => 5})
-  assert_equal expense.amount, 5
-end
 
-def test_has_description
-  expense = Expense.new({:description => "test"})
-  assert_equal expense.description, "test"
-end
+  def test_has_amount
+    expense = Expense.new({:amount => 5})
+    assert_equal expense.amount, 5
+  end
+
+  def test_has_description
+    expense = Expense.new({:description => "test"})
+    assert_equal expense.description, "test"
+  end
 
 end
